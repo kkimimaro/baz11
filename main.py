6975e494ef7cc9c34326dd7db5b26762da30d9f3
Daniil <55910193+kkimimaro@users.noreply.github.com>
2022-04-05 19:20:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,30 +3,80 @@ import os
 from flask import Flask, request
 from telebot import types
 
+import qrcode
 import telebot
+from gtts import gTTS
 
 TOKEN = '5286040884:AAGJ5Qx-2uc4tu0mCJ9ewN4cWdRoWQptbTg'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
-@bot.message_handler(commands=['test'])
-def start_message(message):
-    keyboard = types.InlineKeyboardMarkup() # создаём клавиатуру
-    button1 = types.InlineKeyboardButton(text='Всё супер', callback_data=1) # создаём кнопки
-    button2 = types.InlineKeyboardButton(text='Всё хорошо', callback_data=2) # создаём кнопки
-    keyboard.add(button1) # добавляем кнопки к клавиатуре
-    keyboard.add(button2) # добавляем кнопки к клавиатуре
-    bot.send_message(message.chat.id, text="Привет, как дела?", reply_markup=keyboard)
+@bot.message_handler(commands=['start']) 
+def start(message):
+  keyboard = types.ReplyKeyboardMarkup()
+  keyboard.add("qr код")
+  keyboard.add("аудио")
+  bot.send_message(message.chat.id, 'тмукг', reply_markup= keyboard)
+  bot.register_next_step_handler(message, make)
 
-    
-@bot.callback_query_handler(func=lambda call: True)
-def query_handler(call):
-    bot.answer_callback_query(callback_query_id=call.id) #text='Спасибо за честный ответ!'
-    if call.data == '1':
-      bot.send_message(call.message.chat.id, "Вы нажали на кнопку №1")
-    elif call.data == '2':
-      bot.send_message(call.message.chat.id, "Вы нажали на кнопку №2")
+def make(message):
+  if message.text=="аудио":
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.add("mp3")
+    keyboard.add("голосовое сообщение")
+    bot.send_message(message.chat.id, 'выбирете, как отправить аудио', reply_markup= keyboard)
+    bot.register_next_step_handler(message, audiochoice)
+  if message.text=="qr код":
+    bot.send_message(message.chat.id, 'введите текст для qr кода')
+    bot.register_next_step_handler(message, qrtext)
+  
+def audiochoice(message):
+  if message.text=="mp3":
+    bot.register_next_step_handler(message, makeaudio_mp3)
+  if message.text=="голосовое сообщение":
+    bot.register_next_step_handler(message, makeaudio_voise)
+
+
+def qrtext(message):
+  qrcodetext = message.text  #message.text забирает текст, который ввел пользователь  нужна отдельная ветка
+  image = qrcode.make(qrcodetext)
+  image.save('qr.png')
+  img = open('qr.png','rb')
+  bot.send_photo(message.from_user.id, img)
+  img.close()
+
+def makeaudio_mp3(message):
+  bot.send_message(message.chat.id, 'введите текст для создания аудио файла')
+  bot.register_next_step_handler(message, audiotextmp3)
+
+def audiotextmp3(message):
+  audiotext = message.text  #message.text забирает текст, который ввел пользователь  нужна отдельная ветка
+  audio = gTTS(audiotext, lang= 'ru')
+  audio.save('audio.mp3')
+  aud = open('audio.mp3','rb')
+  bot.send_audio(message.from_user.id, aud)
+  aud.close()
+
+def makeaudio_voise(message):
+  bot.send_message(message.chat.id, 'введите текст для создания аудио файла')
+  bot.register_next_step_handler(message, audiotextvoice)
+
+def audiotextvoice(message):
+  audiotext = message.text  #message.text забирает текст, который ввел пользователь  нужна отдельная ветка
+  audio = gTTS(audiotext, lang= 'ru')
+  audio.save('audio.mp3')
+  aud = open('audio.mp3','rb')
+  bot.send_voice(message.from_user.id, aud)
+  aud.close()
+  
+  
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
     
     
 @server.route('/' + TOKEN, methods=['POST'])
