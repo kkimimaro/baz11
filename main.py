9ef5b1b741e65783c4b47347c3513d552ec567ee
Daniil <55910193+kkimimaro@users.noreply.github.com>
2022-04-05 18:49:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ from telebot import types
 
 import telebot
 
-TOKEN = 'ВАШ ТОКЕН'
+TOKEN = '5286040884:AAGJ5Qx-2uc4tu0mCJ9ewN4cWdRoWQptbTg'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
-#КОД ТЕЛЕБОТА
+@bot.message_handler(commands=['test'])
+def start_message(message):
+    keyboard = types.InlineKeyboardMarkup() # создаём клавиатуру
+    button1 = types.InlineKeyboardButton(text='Всё супер', callback_data=1) # создаём кнопки
+    button2 = types.InlineKeyboardButton(text='Всё хорошо', callback_data=2) # создаём кнопки
+    keyboard.add(button1) # добавляем кнопки к клавиатуре
+    keyboard.add(button2) # добавляем кнопки к клавиатуре
+    bot.send_message(message.chat.id, text="Привет, как дела?", reply_markup=keyboard)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
